e7adec455ca6de4978e5439e44163febefc065f0
Thingk0 <audtjd9080@naver.com>
2023-07-29 16:51:06 +0900

    :sparkles: Member 엔티티 추가.
    - email, nickname 필드는 중복이 불가능하도록 하기 위해서 unique 옵션 추가.
    - 프로필 이미지는 인덱스 값을 통해서 조회하기 위해서 int 타입으로 필드 추가.

---
+++ src/main/java/org/goodpeoplegoodtimes/domain/Member.java
@@ -0,0 +1,31 @@
+package org.goodpeoplegoodtimes.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    private int imgNum;
+
+}
