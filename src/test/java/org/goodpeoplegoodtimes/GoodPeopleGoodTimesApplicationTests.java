10bce3997fec990b5c4ac2e9276db9e2b00e0f7b
Thingk0 <audtjd9080@naver.com>
2023-07-29 15:20:41 +0900

    :tada: Good-People-Good-Times Project

---
+++ src/test/java/org/goodpeoplegoodtimes/GoodPeopleGoodTimesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.goodpeoplegoodtimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoodPeopleGoodTimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
