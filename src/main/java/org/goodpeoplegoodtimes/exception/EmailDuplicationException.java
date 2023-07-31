c8fe6c16133e29c46e9c8abb874b0ba14abdbffe
mlngwan <david027301@naver.com>
2023-08-01 02:02:00 +0900

    :sparkles: 중복확인 및 예외처리 설정
    - 이메일 찾기 중복확인 및 예외처리 설정
    - 닉네임 찾기 중복확인 및 예외처리 설정

---
+++ src/main/java/org/goodpeoplegoodtimes/exception/EmailDuplicationException.java
@@ -0,0 +1,7 @@
+package org.goodpeoplegoodtimes.exception;
+
+public class EmailDuplicationException extends RuntimeException{
+    public EmailDuplicationException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/org/goodpeoplegoodtimes/exception/NicknameDuplicationException.java
@@ -0,0 +1,7 @@
+package org.goodpeoplegoodtimes.exception;
+
+public class NicknameDuplicationException extends RuntimeException{
+    public NicknameDuplicationException(String message) {
+        super(message);
+    }
+}
