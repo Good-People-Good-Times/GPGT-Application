fd00f48e3c7709fcdc63195f35c658b581ce3142
박인영 <130139501+inyouzg@users.noreply.github.com>
2023-08-06 03:05:55 +0900

    Revert "파티 페이지 로직 구현 & 레이아웃 경로 수정"

--- src/main/java/org/goodpeoplegoodtimes/exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package org.goodpeoplegoodtimes.exception;
-
-public class EmailAlreadyExistsException extends RuntimeException{
-    public EmailAlreadyExistsException(String message) {
-        super(message);
-    }
-}
