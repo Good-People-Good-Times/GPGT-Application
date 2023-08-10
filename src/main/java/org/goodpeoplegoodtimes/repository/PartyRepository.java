98af2d568d04e6bad39a684bbed2811886d862b3
Thingk0 <audtjd9080@naver.com>
2023-08-10 21:34:49 +0900

    :sparkles: 파티 리스트 조회 구현.
    - 페이징을 적용해서 파티 리스트를 가져오도록 구현.

--- src/main/java/org/goodpeoplegoodtimes/repository/PartyRepository.java
+++ src/main/java/org/goodpeoplegoodtimes/repository/PartyRepository.java
@@ -1,7 +1,21 @@
 package org.goodpeoplegoodtimes.repository;
 
 import org.goodpeoplegoodtimes.domain.Party;
+import org.goodpeoplegoodtimes.domain.dto.party.response.PartyListResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 
 public interface PartyRepository extends JpaRepository <Party, Long> {
+
+    /**
+     * 파티 리스트를 페이징 적용하여 Dto로 반환.
+     * @param pageable -> 페이징 객체
+     */
+    @Query("select new org.goodpeoplegoodtimes.domain.dto.party.response.PartyListResponseDto" +
+        "(p.title, p.category, p.status, p.createdAt) from Party p order by p.createdAt desc")
+    Page<PartyListResponseDto> fetchPartyList(Pageable pageable);
+
 }
