7de6f0aef2ae9667f3886b806201a613a9016609
mlngwan <david027301@naver.com>
2023-08-16 02:35:05 +0900

    :memo: 파티 서비스 코드 재빌딩
    - 검색 조건에 따라 파티 검색 가능하도록 구현
    - 카테고리 별로 검색 가능하도록 구현

--- src/main/java/org/goodpeoplegoodtimes/service/PartyService.java
+++ src/main/java/org/goodpeoplegoodtimes/service/PartyService.java
@@ -3,6 +3,7 @@ package org.goodpeoplegoodtimes.service;
 import lombok.RequiredArgsConstructor;
 import org.goodpeoplegoodtimes.domain.Member;
 import org.goodpeoplegoodtimes.domain.Party;
+import org.goodpeoplegoodtimes.domain.constant.Category;
 import org.goodpeoplegoodtimes.domain.dto.party.request.PartyForm;
 import org.goodpeoplegoodtimes.domain.dto.party.response.PartyDetailResponseDto;
 import org.goodpeoplegoodtimes.domain.dto.party.response.PartyListResponseDto;
@@ -10,32 +11,38 @@ import org.goodpeoplegoodtimes.exception.party.PartyNotFoundException;
 import org.goodpeoplegoodtimes.repository.PartyRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.lang.Nullable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PartyService {
 
     private final MemberService memberService;
     private final PartyRepository partyRepository;
 
-    public Long save(PartyForm partyForm, Authentication authentication) {
+    @Transactional
+    public Long createParty(PartyForm partyForm, Authentication authentication) {
         // 회원 조회
         Member member = memberService.getMember(authentication.getName());
         return partyRepository.save(Party.of(partyForm, member)).getId();
     }
 
-    @Transactional(readOnly = true)
-    public Page<PartyListResponseDto> getPartyList(@Nullable String cond, Pageable pageable) {
-        if (!cond.isEmpty()) return partyRepository.fetchPartyListBySearch(cond, pageable);
+    public Page<PartyListResponseDto> getPartyList(Pageable pageable) {
         return partyRepository.fetchPartyList(pageable);
     }
 
-    @Transactional(readOnly = true)
+
+    public Page<PartyListResponseDto> getPartyList(String cond, Pageable pageable) {
+        return partyRepository.fetchPartyListBySearch(cond, pageable);
+    }
+
+    public Page<PartyListResponseDto> getPartyList(Category category, Pageable pageable) {
+        return partyRepository.fetchPartyListByCategory(category, pageable);
+    }
+
     public PartyDetailResponseDto findPartyDetailById(Long id) {
         return partyRepository.fetchPartyDetail(id).orElseThrow(
             () -> new PartyNotFoundException("파티를 찾을 수 없습니다.")
