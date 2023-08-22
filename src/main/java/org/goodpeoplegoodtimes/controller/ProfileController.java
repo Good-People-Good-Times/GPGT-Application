87e53307bed22fa54635a490b39564939d283325
mlngwan <david027301@naver.com>
2023-08-23 02:21:12 +0900

    :memo: 상대 프로필 페이지 수정
    - 작성한 글을 연결을 위해 컨트롤러 수정
    - 상대 프로필 페이지 레이아웃 수정

--- src/main/java/org/goodpeoplegoodtimes/controller/ProfileController.java
+++ src/main/java/org/goodpeoplegoodtimes/controller/ProfileController.java
@@ -2,24 +2,35 @@ package org.goodpeoplegoodtimes.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.goodpeoplegoodtimes.service.MemberService;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.goodpeoplegoodtimes.service.PartyService;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping(value = "/profile")
+
 public class ProfileController {
 
     private final MemberService memberService;
+    private final PartyService partyService;
 
     @GetMapping(value = "/my_profile")
-    public String displayMyProfile() {
+    public String displayMyProfile(@RequestParam(value = "cond", required = false) String cond,
+                                   Authentication authentication, Model model) {
+        model.addAttribute("myPartyList", partyService.getMyPartyList(authentication.getName(), cond));
         return "profile/my_profile";
     }
 
+
     @GetMapping(value = "/your_profile")
-    public String displayOtherUserProfile() {
+    public String displayOtherUserProfile(@RequestParam(value = "cond", required = false) String cond,
+                                          Authentication authentication, Model model) {
+        model.addAttribute("myPartyList", partyService.getMyPartyList(authentication.getName(), cond));
         return "profile/your_profile";
     }
 
